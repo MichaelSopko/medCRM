1561dc1ebf3f6201d24aeac779c6b7fa3d9fac35
Anton S <spaxi4@yandex.ru>
2017-02-28 20:37:33 +0300

    Css tag

--- src/ui/components/Html.jsx
+++ src/ui/components/Html.jsx
@@ -10,13 +10,22 @@ const Html = ({ content, state, assetMap }) => {
 			<meta charSet="utf-8"/>
 			<meta name="viewport" content="width=device-width, initial-scale=1"/>
 			<title>Clinic</title>
-			{!__DEV__ && <link rel="stylesheet" type="text/css" href={`/assets/${assetMap['bundle.css']}`}/>}
+
+			<link rel="apple-touch-icon" sizes="180x180" href={`${assetMap["apple-touch-icon.png"]}`}/>
+			<link rel="icon" type="image/png" href={`${assetMap["favicon-32x32.png"]}`} sizes="32x32"/>
+			<link rel="icon" type="image/png" href={`${assetMap["favicon-16x16.png"]}`} sizes="16x16"/>
+			<link rel="manifest" href={`${assetMap["manifest.json"]}`}/>
+			<link rel="mask-icon" href={`${assetMap["safari-pinned-tab.svg"]}`} color="#5bbad5"/>
+			<link rel="shortcut icon" href={`${assetMap["favicon.ico"]}`} />
+			<meta name="msapplication-config" content={`${assetMap["browserconfig.xml"]}`} />
+			<meta name="theme-color" content="#ffffff"/>
+			{!__DEV__ && <link rel="stylesheet" type="text/css" href={`${assetMap['bundle.css']}`} />}
 			{__DEV__ &&
-			<style dangerouslySetInnerHTML={{
-				__html: require('../styles.scss')._getCss()
+			<style dangerouslySetInnerHTML={{ __html:
+				require('../styles.scss')._getCss()
 			}}/>
 			}
-			<link rel="shortcut icon" href="/favicon.ico"/>
+
 		</head>
 		<body className={ cx({ 'rtl': isRtl }) }>
 		<div id="content" dangerouslySetInnerHTML={{ __html: content }}/>
