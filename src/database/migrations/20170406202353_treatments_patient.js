adb545411e303e051c2d055b8d4d7e441356d4e5
Anton S <spaxi4@yandex.ru>
2017-04-08 00:12:10 +0300

    migration fix

--- src/database/migrations/20170406202353_treatments_patient.js
+++ src/database/migrations/20170406202353_treatments_patient.js
@@ -1,6 +1,6 @@
 export function up(knex, Promise) {
-	return knex.transaction(trx => Promise.all([
-		trx.schema.table('users', (table) => {
+	return knex.transaction(trx => trx
+		.schema.table('users', (table) => {
 			// table.specificType('diagnoses', 'JSON');
 			// table.specificType('treatment_summary', 'JSON');
 			table.text('diagnoses');
@@ -8,24 +8,24 @@ export function up(knex, Promise) {
 			table.boolean('disabled').defaultTo(false);
 			table.boolean('archived').index().defaultTo(false);
 			table.dateTime('archived_date');
-		}),
-		trx.schema.table('clinics', (table) => {
+		})
+		.table('clinics', (table) => {
 			table.integer('treatment_duration').unsigned().defaultTo(60);
 			table.integer('patients_limit').unsigned().defaultTo(100);
 			table.integer('archive_time').unsigned().defaultTo(60);
 			table.boolean('disabled').defaultTo(false);
 			table.boolean('deleted').index().defaultTo(false);
-		}),
-		trx.schema.table('treatment_series', (table) => {
+		})
+		.table('treatment_series', (table) => {
 			table.integer('patient_id').unsigned().references('id').inTable('users');
-		}),
-		trx.schema.table('treatments', (table) => {
+		})
+		.table('treatments', (table) => {
 			table.dropColumn('date');
 			table.dropColumn('patient_ids');
 			table.dateTime('start_date');
 			table.dateTime('end_date');
 		}),
-	]));
+	);
 }
 
 export function down() {
