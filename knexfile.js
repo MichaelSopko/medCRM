accf9427d98886615aeef7af592ca46f9adf7957
Sharon Haim <hpsharon@gmail.com>
2017-08-05 12:02:36 +0300

    knexfile - DB name

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,7 @@ module.exports = {
 			host: process.env.DATABASE_HOST || '127.0.0.1',
 			user: process.env.DATABASE_USER || 'root',
 			password: process.env.DATABASE_PASSWORD || '',
-			database: process.env.DATABASE_NAME || 'clinic-prod'
+			database: process.env.DATABASE_NAME || 'clinic-app'
 		},
 		seeds: {
 			directory: './src/database/seeds'
