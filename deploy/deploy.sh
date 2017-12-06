5af81734eaf3fa3f371dfdde533ce0e8ead242e7
romashka50 <roma.buchuk@gmail.com>
2017-12-07 00:18:47 +0200

    added deploy script;

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,5 +5,6 @@ systemctl stop clinic.service
 cp /home/clinic/jobs/petro-clinic/deploy/clinic.service /etc/systemd/system/clinic.service
 
 systemctl daemon-reload
+systemctl enable clinic.service
 
 systemctl start clinic.service
