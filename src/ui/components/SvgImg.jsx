15697cca6896fde96218f4a614166f943e8e5495
romashka50 <roma.buchuk@gmail.com>
2017-12-31 21:42:18 +0200

    changed styles for ie blur;

--- src/ui/components/SvgImg.jsx
+++ src/ui/components/SvgImg.jsx
@@ -4,7 +4,7 @@ import './SvgImg.scss';
 
 const SvgImageFilter = (props) => {
   const filterStrength = props.filterStrength;
-  const svgFilter = `<<defs>
+  const svgFilter = `<defs>
                                   <filter id="blurlayer">
                                     
                                     <feGaussianBlur  stdDeviation="${filterStrength}" result="blur"/>
