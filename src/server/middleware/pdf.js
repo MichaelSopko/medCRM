148e19ca5211979adc37443c3d7d3226250bc36f
michael <michael.sopko@ciklum.com>
2018-06-23 18:26:07 +0300

    connection

--- src/server/middleware/pdf.js
+++ src/server/middleware/pdf.js
@@ -1,26 +1,29 @@
-import * as pdf  from 'html-pdf';
+import * as pdf from 'html-pdf';
 import * as fs from 'fs';
 import * as _ from 'underscore';
-import { app as settings } from '../../../package.json';
 import User from '../sql/models/users';
 
 const user = new User();
-const pdfTemplate = fs.readFileSync('./templates/treaatment-summary-pdf', 'utf-8');
+const pdfTemplate = fs.readFileSync('./templates/treaatment-summary-pdf.html', 'utf-8');
 
 export default (req, res, next) => {
     const params = req.params;
-    const timestamp = new Date().getTime();
     const template = _.template(pdfTemplate);
     
     user.getTreatmentSummaryById(params.recordId).then((result) => {
-        console.log(result);
-        console.log(params);
-        console.log(`${settings.uploadsDir}/${timestamp}`);
+        const data = result[0];
+
+        console.log(data);
         
-        // pdf.create(template({}), {format: 'A3', orientation: 'portrait'}).toStream(() => {
-        //
-        // });
+        pdf.create(template({ data }), { format: 'A3', orientation: 'portrait' }).toStream((err, stream) => {
+            if (err) {
+                return next(err);
+            }
+            
+            res.setHeader('Content-disposition', 'inline');
+            res.setHeader('Content-Type', 'application/pdf; charset=utf-8');
     
-        res.send(template({}));
+            return stream.pipe(res);
+        });
     }).catch(err => res.status(400).send(err));
 };
